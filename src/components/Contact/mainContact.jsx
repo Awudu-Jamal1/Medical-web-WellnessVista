2e4fe6a955b37420ee7b7ea7fabf46b60bbe1245
Awudu Jamal <78368357+Awudu-Jamal1@users.noreply.github.com>
2025-01-03 11:34:23 +0000

    meth contact

--- src/components/Contact/mainContact.jsx
+++ src/components/Contact/mainContact.jsx
@@ -87,7 +87,7 @@ function MainContact() {
                         <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"></path>
                       </svg>
                       <p className="block antialiased font-sans text-base leading-relaxed text-inherit font-bold">
-                        info@murphyehealth.com
+                      info@mehtltd.co.uk
                       </p>
                     </div>
                     {/* <div className="flex items-center gap-4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-6 w-6">
